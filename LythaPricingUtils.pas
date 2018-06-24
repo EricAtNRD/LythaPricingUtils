e62e2d6740050e89e0bd3d33fee877f1c73f3990
Eric Weik <ericw@newriversdigital.com>
2018-06-24 15:18:50 -0400

    Apply updates to project file structure and units.

--- LythaPricingUtils.pas
+++ LythaPricingUtils.pas
@@ -7,7 +7,7 @@ uses
   cthreads,
   {$ENDIF}{$ENDIF}
   Interfaces, // this includes the LCL widgetset
-  Forms, LythaPricingUtilsFormUnit
+  Forms, FormUnit
   { you can add units after this };
 
 {$R *.res}
